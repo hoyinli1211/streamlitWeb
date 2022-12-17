5efff214f0023a6a04ed8dcb34fa90cd5f6de121
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-17 17:02:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from streamlit_option_menu import option_menu
 with st.sidebar:
     selected = option_menu(None, ["Home", 'Upload', 'Tasks', 'Settings'], 
                            icons=['house', 'cloud-upload', 'list-task', 'gear'], 
-                           menu_icon="cast", default_index=0, orientation='horizontal')
+                           menu_icon="cast", default_index=0, orientation='vertical')
     selected
